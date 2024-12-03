7abe9058117ef9023b3d734b61281864ccd34d88
Numer <numerpriv@gmail.com>
2024-12-03 15:07:23 +0100

    feat:added GroupButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import GroupButton from "@/app/components/Menu/GroupButton";
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <GroupButton />
+    </>
+  );
 };
 
 export default Home;
